4158199d52bcfc1c0b061422daae4b31eb045f65
Nasief Khan <nasiefk14@gmail.com>
2023-05-09 13:25:19 +0200

    Change File Structure

---
+++ Ecommerce-App/resolvers/Query.js
@@ -0,0 +1,17 @@
+const { categories, products } = require('../db')
+
+exports.Query = {
+    hello: (parent, args, context) => 'Hey Nasief!',
+    products: (parent, args, context) => products,
+    product: (parent, args, context) => {
+        const productId = args.id
+        const product = products.find(product => product.id === productId)
+        if (!products) return null
+        return product
+    },
+    categories: (parent, args, context) => categories,
+    category: (parent, args, context) => {
+        const { id } = args;
+        return categories.find((category) => category.id === id)
+    }
+}
